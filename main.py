af132c588faf1806b66fc6809d50ecdbbf033891
prattjoel <pratt.joel@gmail.com>
2016-01-09 11:32:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ def _GuestbookKey(guestbook_name=None):
 
 class LessonHandler(Handler):
 
-    def get(self, template_file, guestbook):
-        """Handle GET requests."""
+    def page(self, template_file, guestbook):
+        """Generates page"""
         guestbook_name = self.request.get('guestbook_name', guestbook)
         greetings_query = Greeting.all().ancestor(
             _GuestbookKey(guestbook_name)).order('-date')
@@ -74,8 +74,8 @@ class LessonHandler(Handler):
         self.render(template_file, template_values)
 
 
-    def post(self, guestbook):
-        """Handle POST requests."""
+    def comment(self, guestbook, redirection):
+        """Generates Comments"""
         guestbook_name = self.request.get('guestbook_name', guestbook)
         greeting = Greeting(parent=_GuestbookKey(guestbook_name))
 
@@ -89,294 +89,97 @@ class LessonHandler(Handler):
         else:
             error = "Please enter text to leave a comment"
             query_params = {'guestbook_name': guestbook_name, "error": error}
-        self.redirect('/?' + urllib.urlencode(query_params))
+        self.redirect(redirection + urllib.urlencode(query_params))
 
 
 
 
 class MainPage(LessonHandler):
     
-    def page_one(self):
+    def get(self):
         guestbook_one = 'lesson_one_guestbook'
+        page_one = 'the_web.html'
         # self.render('the_web.html', template_values)
-        self.get('the_web.html', guestbook_one)
+        self.page(page_one, guestbook_one)
 
-    # def get(self):
-    #     """Handle GET requests."""
-    #     guestbook_name = self.request.get('guestbook_name', guestbook_one)
-    #     greetings_query = Greeting.all().ancestor(
-    #         _GuestbookKey(guestbook_name)).order('-date'),l l
-    #     num_greetings = 10
-    #     greetings = greetings_query.fetch(num_greetings)
-    #     error = self.request.get("error")
 
-    #     if users.get_current_user():
-    #         url = users.create_logout_url(self.request.uri)
-    #         url_linktext = 'Logout'
-    #     else:
-    #         url = users.create_login_url(self.request.uri)
-    #         url_linktext = 'Login'
+    def post(self):
+        guestbook_one = 'lesson_one_guestbook'
+        lesson_one_link = '/?'
+        self.comment(guestbook_one, lesson_one_link)
 
-    #     template_values = {
-    #         'greetings': greetings,
-    #         'guestbook_name': urllib.quote_plus(guestbook_name),
-    #         'url': url,
-    #         'url_linktext': url_linktext,
-    #         "error": error,
-    #     }
-
-    #     self.render("the_web.html", template_values)
-
-    # def post(self):
-    #     """Handle POST requests."""
-    #     guestbook_name = self.request.get('guestbook_name', guestbook_one)
-    #     greeting = Greeting(parent=_GuestbookKey(guestbook_name))
-
-    #     if users.get_current_user():
-    #         greeting.author = users.get_current_user().nickname()
-
-    #     greeting.content = self.request.get('content')
-    #     if greeting.content.strip():
-    #         greeting.put()
-    #         query_params = {'guestbook_name': guestbook_name, }
-    #     else:
-    #         error = "Please enter text to leave a comment"
-    #         query_params = {'guestbook_name': guestbook_name, "error": error}
-    #     self.redirect('/?' + urllib.urlencode(query_params))
 
 guestbook_two = 'lesson_two_guestbook'
 
-
-class LessonTwo(Handler):
+class LessonTwo(LessonHandler):
     def get(self):
         """Handle GET requests."""
-        guestbook_name = self.request.get('guestbook_name', guestbook_two)
-        greetings_query = Greeting.all().ancestor(
-            _GuestbookKey(guestbook_name)).order('-date')
-        num_greetings = 10
-        greetings = greetings_query.fetch(num_greetings)
-        error = self.request.get("error")
-
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        template_values = {
-            'greetings': greetings,
-            'guestbook_name': urllib.quote_plus(guestbook_name),
-            'url': url,
-            'url_linktext': url_linktext,
-            "error": error,
-        }
-        self.render("structure.html", template_values)
+        page_two = "structure.html"
+        self.page(page_two, guestbook_two)
 
     def post(self):
         """Handle POST requests."""
-        guestbook_name = self.request.get('guestbook_name', guestbook_two)
-        greeting = Greeting(parent=_GuestbookKey(guestbook_name))
-
-        if users.get_current_user():
-            greeting.author = users.get_current_user().nickname()
-
-        greeting.content = self.request.get('content')
-        if greeting.content.strip():
-            greeting.put()
-            query_params = {'guestbook_name': guestbook_name, }
-        else:
-            error = "Please enter text to leave a comment"
-            query_params = {'guestbook_name': guestbook_name, "error": error}
-        self.redirect('/structure?' + urllib.urlencode(query_params))
-
+        lesson_two_link = '/structure?'
+        self.comment(guestbook_two, lesson_two_link)
+  
 guestbook_three = 'lesson_three_guestbook'
 
 
-class LessonThree(Handler):
+class LessonThree(LessonHandler):
     def get(self):
         """Handle GET requests."""
-        guestbook_name = self.request.get('guestbook_name', guestbook_three)
-        greetings_query = Greeting.all().ancestor(
-            _GuestbookKey(guestbook_name)).order('-date')
-        num_greetings = 10
-        greetings = greetings_query.fetch(num_greetings)
-        error = self.request.get("error")
-
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        template_values = {
-            'greetings': greetings,
-            'guestbook_name': urllib.quote_plus(guestbook_name),
-            'url': url,
-            'url_linktext': url_linktext,
-            "error": error,
-        }
-
-        self.render("styling.html", template_values)
+        page_three = "styling.html"
+        self.page(page_three, guestbook_three)
 
     def post(self):
         """Handle POST requests."""
-        guestbook_name = self.request.get('guestbook_name', guestbook_three)
-        greeting = Greeting(parent=_GuestbookKey(guestbook_name))
-
-        if users.get_current_user():
-            greeting.author = users.get_current_user().nickname()
-
-        greeting.content = self.request.get('content')
-        if greeting.content.strip():
-            greeting.put()
-            query_params = {'guestbook_name': guestbook_name, }
-        else:
-            error = "Please enter text to leave a comment"
-            query_params = {'guestbook_name': guestbook_name, "error": error}
-        self.redirect('/styling?' + urllib.urlencode(query_params))
+        lesson_three_link = '/styling?'
+        self.comment(guestbook_three, lesson_three_link)
 
 guestbook_four = 'servers_guestbook'
 
 
-class ServersContent(Handler):
+class ServersContent(LessonHandler):
     def get(self):
         """Handle GET requests."""
-        guestbook_name = self.request.get('guestbook_name', guestbook_four)
-        greetings_query = Greeting.all().ancestor(
-            _GuestbookKey(guestbook_name)).order('-date')
-        num_greetings = 10
-        greetings = greetings_query.fetch(num_greetings)
-        error = self.request.get("error")
-
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        template_values = {
-            'greetings': greetings,
-            'guestbook_name': urllib.quote_plus(guestbook_name),
-            'url': url,
-            'url_linktext': url_linktext,
-            "error": error,
-        }
-
-        self.render("servers_content.html", template_values)
+        page_four = "servers_content.html"
+        self.page(page_four, guestbook_four)
 
     def post(self):
         """Handle POST requests."""
-        guestbook_name = self.request.get('guestbook_name', guestbook_four)
-        greeting = Greeting(parent=_GuestbookKey(guestbook_name))
-
-        if users.get_current_user():
-            greeting.author = users.get_current_user().nickname()
-
-        greeting.content = self.request.get('content')
-        if greeting.content.strip():
-            greeting.put()
-            query_params = {'guestbook_name': guestbook_name, }
-        else:
-            error = "Please enter text to leave a comment"
-            query_params = {'guestbook_name': guestbook_name, "error": error}
-        self.redirect('/servers?' + urllib.urlencode(query_params))
-
+        lesson_four_link = '/servers?'
+        self.comment(guestbook_four, lesson_four_link)
+       
 guestbook_five = 'validating_input'
 
 
-class ValidInput(Handler):
+class ValidInput(LessonHandler):
     def get(self):
         """Handle GET requests."""
-        guestbook_name = self.request.get('guestbook_name', guestbook_five)
-        greetings_query = Greeting.all().ancestor(
-            _GuestbookKey(guestbook_name)).order('-date')
-        num_greetings = 10
-        greetings = greetings_query.fetch(num_greetings)
-        error = self.request.get("error")
-
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        template_values = {
-            'greetings': greetings,
-            'guestbook_name': urllib.quote_plus(guestbook_name),
-            'url': url,
-            'url_linktext': url_linktext,
-            "error": error,
-        }
-
-        self.render("valid_input.html", template_values)
-
+        page_five = "valid_input.html"
+        self.page(page_five, guestbook_five)
+   
     def post(self):
         """Handle POST requests."""
-        guestbook_name = self.request.get('guestbook_name', guestbook_five)
-        greeting = Greeting(parent=_GuestbookKey(guestbook_name))
-
-        if users.get_current_user():
-            greeting.author = users.get_current_user().nickname()
-
-        greeting.content = self.request.get('content')
-        if greeting.content.strip():
-            greeting.put()
-            query_params = {'guestbook_name': guestbook_name, }
-        else:
-            error = "Please enter text to leave a comment"
-            query_params = {'guestbook_name': guestbook_name, "error": error}
-        self.redirect('/validation?' + urllib.urlencode(query_params))
+        lesson_five_link = '/validation?'
+        self.comment(guestbook_five, lesson_five_link)
+       
 
 guestbook_six = 'templates'
 
 
-class UsingTemplates(Handler):
+class UsingTemplates(LessonHandler):
     def get(self):
         """Handle GET requests."""
-        guestbook_name = self.request.get('guestbook_name', guestbook_six)
-        greetings_query = Greeting.all().ancestor(
-            _GuestbookKey(guestbook_name)).order('-date')
-        num_greetings = 10
-        greetings = greetings_query.fetch(num_greetings)
-        error = self.request.get("error")
-
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        template_values = {
-            'greetings': greetings,
-            'guestbook_name': urllib.quote_plus(guestbook_name),
-            'url': url,
-            'url_linktext': url_linktext,
-            "error": error,
-        }
-
-        self.render("using_templates.html", template_values)
+        page_six = "using_templates.html"
+        self.page(page_six, guestbook_six)
+       
 
     def post(self):
         """Handle POST requests."""
-        guestbook_name = self.request.get('guestbook_name', guestbook_six)
-        greeting = Greeting(parent=_GuestbookKey(guestbook_name))
-
-        if users.get_current_user():
-            greeting.author = users.get_current_user().nickname()
-
-        greeting.content = self.request.get('content')
-        if greeting.content.strip():
-            greeting.put()
-            query_params = {'guestbook_name': guestbook_name, }
-        else:
-            error = "Please enter text to leave a comment"
-            query_params = {'guestbook_name': guestbook_name, "error": error}
-        self.redirect('/templates?' + urllib.urlencode(query_params))
+        lesson_six_link = '/templates?'
+        self.comment(guestbook_six, lesson_six_link)
+  
 
 
 app = webapp2.WSGIApplication([
